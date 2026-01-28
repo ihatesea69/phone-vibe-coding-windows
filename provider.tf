0ea6935b30dd88fe124342e8348e2f2cd895b2e3
nghidanh <nghi.danh@renovacloud.com>
2026-01-28 23:37:33 +0700

    Fix: Add Owner tag for IAM policy compliance

--- provider.tf
+++ provider.tf
@@ -10,4 +10,11 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
+
+  default_tags {
+    tags = {
+      Owner   = "nghi-danh"
+      Project = "PhoneVibeCoding"
+    }
+  }
 }
